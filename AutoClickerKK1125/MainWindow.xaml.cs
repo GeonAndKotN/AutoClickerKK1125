87e6608131d7ff067663d78f89891fa2fd2ffd73
Student <Student@CAB4-8>
2024-01-25 10:06:17 +1000

    Добавьте файлы проекта.

---
+++ AutoClickerKK1125/MainWindow.xaml.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using WindowsInput;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Threading;
+using System.Windows.Automation;
+
+namespace AutoClickerKK1125
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        static int am;
+        static int ms;
+
+        private void Znachenia()
+        {
+            am = Int32.Parse(Amount.Text);
+            ms = Int32.Parse(TimeInSeconds.Text); 
+        }
+        Thread thread;
+
+        private void StartClick(object sender, RoutedEventArgs e)
+        {
+            Znachenia();
+          thread = new Thread( Anotherthread);
+            thread.Start();
+        }
+
+        private void Anotherthread()
+        {
+            InputSimulator simulator = new InputSimulator();
+            for (int i = 0; i < am; i++)
+            {
+                simulator.Mouse.LeftButtonClick();
+                Thread.Sleep(ms);
+            }
+        }
+
+        private void StopClick(object sender, RoutedEventArgs e)
+        {
+            am = 0;
+        }
+
+        private void TextBox_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            TextBox textBox = sender as TextBox;
+            if (textBox.Text == textBox.ToolTip.ToString())
+                textBox.Text = null;
+        }
+    }
+}
